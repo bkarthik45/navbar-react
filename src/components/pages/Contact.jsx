8123723786405176b9c8e37c5b39c766effa3aec
Karthik Reddy Kallu <karthikreddykallu@Laptop.attlocal.net>
2024-10-09 23:55:12 -0400

    Added new features to the login page

--- src/components/pages/Contact.jsx
+++ src/components/pages/Contact.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Contact = () => {
   return (
     <div>
-      Contact
+      This is Contact page 
     </div>
   )
 }
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-      Home
+      This is Home page
     </div>
   )
 }
